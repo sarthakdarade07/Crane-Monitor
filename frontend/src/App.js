ed9c0a2376db138383d19fbfd3f522c1e3781969
sarthakdarade07 <sarthakdarade07@gmail.com>
2026-06-21 20:42:16 +0530

    Removed unused useEffect import to fix Netlify CI build error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import Login from './components/Login';
